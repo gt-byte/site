2587f3f34c44706b6cd357966475ab8b2821c750
uenowataru <ueno.wataru@live.com>
2014-04-09 23:37:57 -0400

    feafeaf

--- js/submit.js
+++ js/submit.js
@@ -1,8 +1,5 @@
-var invite_email = "";
-
 $(document).ready(function() {
         fixHeaderHeight();
-
         $('#submit-button').click(function(){
                 var email_field = document.getElementById("input-email");
                 var value =  email_field.form.email.value;
@@ -13,9 +10,8 @@ $(document).ready(function() {
                         var email_field = document.getElementById("input-email");
                         var value =  email_field.form.email.value;
                         submit(value);
-                }     
+                }
         });
-
 });
 
 
@@ -35,23 +31,23 @@ function submit(email){
 
 function showResponse(data){
         if(data==='1'){
-                var response = "<br>You will be notified when Byte is up and running! <br>" + "In the mean time, invite your friends.";
-                document.getElementById("server-response").innerHTML = response ; //+ "<br>"+ data;
-                document.getElementById("server-response").style.color = "#59E817" ;
+                var response = " <span>You will be notified when Byte is up and running!  </span> <span>In the mean time, invite your friends. </span>";
+                document.getElementById("beta-invite").innerHTML = response ; //+ "<br>"+ data;
+                document.getElementById("beta-invite").style.color = "#41A317" ;
                 //showShareLink();
         }else if(data==='2'){
-                var response = "<br>This email is already registered! <br>" + "In the mean time, invite your friends.";
-                document.getElementById("server-response").innerHTML = response ;// + "<br>" + data;
-                document.getElementById("server-response").style.color = "#59E817";
+                var response = " <span> This email is already registered!  </span> <span> In the mean time, invite your friends. </span>";
+                document.getElementById("beta-invite").innerHTML = response ;// + "<br>" + data;
+                document.getElementById("beta-invite").style.color = "#59E817";
                 //showShareLink();
         }else if(data==='0'){
-                var response = "<br>This email is not valid! <br>" + "Please enter a valid email address.";
-                document.getElementById("server-response").innerHTML = response; //+ "<br>"  + data;
-                document.getElementById("server-response").style.color = "#F70D1A";
+                var response = " <span> This email is not valid! </span> <span> Please enter a valid email address. </span>";
+                document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
+                document.getElementById("beta-invite").style.color = "#F70D1A";
         }else{
-                var response = "<br>Error connecting to server! <br>" + "Please try again in a bit.";
-                document.getElementById("server-response").innerHTML = response; //+ "<br>"  + data;
-                document.getElementById("server-response").style.color = "#FDD017";
+                var response = " <span> Error connecting to server! </span> <span>Please try again in a bit.</span>";
+                document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
+                document.getElementById("beta-invite").style.color = "#F70D1A";
         }
         fixHeaderHeight();
         scrollToResponse();
