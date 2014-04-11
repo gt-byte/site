c9f56639407d9b5a1fdfe436b1108af5692991a1
uenowataru <ueno.wataru@live.com>
2014-04-11 13:18:39 -0400

    UI update

--- js/submit.js
+++ js/submit.js
@@ -21,7 +21,6 @@ $(document).ready(function() {
                         scrollTop: window_offset,
                         scrollLeft: 0
                 }, 1000);
-
         });
 
 });
@@ -43,26 +42,31 @@ function submit(email){
 }
 
 function showResponse(data){
-        if(data==='1'){
-                var response = "<span>You will be notified when Byte is up and running!  </span> In the mean time, <a href = \"http://www.facebook.com/sharer.php?u=http://www.datbyte.com\" onclick=\"window.open(this.href, 'Share Byte', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=1'); window.close(); return false;\"> <u><b><font color= \"#3B5999\">invite</font></b></u></a> your friends.";
-                document.getElementById("beta-invite").innerHTML = response ; //+ "<br>"+ data;
-                document.getElementById("beta-invite").style.color = "#41A317" ;
-                //showShareLink();
-        }else if(data==='2'){
-                var response = " <span> This email is already registered!  </span>  In the mean time, <a href = \"http://www.facebook.com/sharer.php?u=http://www.datbyte.com\" onclick=\"window.open(this.href, 'Share Byte', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=1'); window.close(); return false;\"> <u><b><font color= \"#3B5999\">invite</font></b></u></a> your friends.";
-                document.getElementById("beta-invite").innerHTML = response ;// + "<br>" + data;
-                document.getElementById("beta-invite").style.color = "#41A317";
-        }else if(data==='0'){
-                var response = " <span> This email is not valid! </span> <span> Please enter a valid email address. </span>";
-                document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
-                document.getElementById("beta-invite").style.color = "#F70D1A";
-        }else{
-                var response = " <span> Error connecting to server! </span> <span>Please try again in a bit.</span>";
-                document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
-                document.getElementById("beta-invite").style.color = "#F70D1A";
-        }
+        $('#beta-invite').fadeOut( 800, function() {
+                if(data==='1'){
+                        var response = "<span>You will be notified when Byte is up and running!  </span> In the mean time, <a href = \"http://www.facebook.com/sharer.php?u=http://www.datbyte.com\" onclick=\"window.open(this.href, 'Share Byte', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=1'); window.close(); return false;\"> <u><b><font color= \"#3B5999\">invite</font></b></u></a> your friends.";
+                        document.getElementById("beta-invite").innerHTML = response ; //+ "<br>"+ data;
+                        document.getElementById("beta-invite").style.color = "#41A317" ;
+                        //showShareLink();
+                }else if(data==='2'){
+                        var response = " <span> This email is already registered!  </span>  In the mean time, <a href = \"http://www.facebook.com/sharer.php?u=http://www.datbyte.com\" onclick=\"window.open(this.href, 'Share Byte', 'left=20,top=20,width=500,height=500,toolbar=1,resizable=1'); window.close(); return false;\"> <u><b><font color= \"#3B5999\">invite</font></b></u></a> your friends.";
+                        document.getElementById("beta-invite").innerHTML = response ;// + "<br>" + data;
+                        document.getElementById("beta-invite").style.color = "#41A317";
+                }else if(data==='0'){
+                        var response = " <span> This email is not valid! </span> <span> Please enter a valid email address. </span>";
+                        document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
+                        document.getElementById("beta-invite").style.color = "#F70D1A";
+                }else{
+                        var response = " <span> Error connecting to server! </span> <span>Please try again in a bit.</span>";
+                        document.getElementById("beta-invite").innerHTML = response; //+ "<br>"  + data;
+                        document.getElementById("beta-invite").style.color = "#F70D1A";
+                }
+        });
         fixHeaderHeight();
         scrollToResponse();
+        $('#beta-invite').fadeIn( 1000, function() {
+                $( "#beta-invite" ).show();
+        });
 }      
 
 function showShareLink(){
